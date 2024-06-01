3ff085874fa06de1aa06cf64eb05f06a33d10453
Aka2210 <qoo0973215128@gmail.com>
2024-06-01 22:42:39 +0800

    fix: Fix button can't click bug

--- Pages/StorePage.java
+++ Pages/StorePage.java
@@ -13,32 +13,18 @@ import src.*;
 public class StorePage implements ActionListener {
     public static JPanel createStoreMenuPanel() {
         JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 10));
+        StorePage storePage = new StorePage();
 
         JButton goodsPageButton = DataStore.createCustomButton("剩餘商品");
-        goodsPageButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                this.actionPerformed(e);
-            }
-        });
+        goodsPageButton.addActionListener(storePage);
         goodsPageButton.setActionCommand("Goods Page");
 
         JButton inventoryPageButton = DataStore.createCustomButton("材料庫存");
-        inventoryPageButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                this.actionPerformed(e);
-            }
-        });
+        inventoryPageButton.addActionListener(storePage);
         inventoryPageButton.setActionCommand("Inventory Page");
 
         JButton financeReportButton = DataStore.createCustomButton("財務報表");
-        financeReportButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                this.actionPerformed(e);
-            }
-        });
+        financeReportButton.addActionListener(storePage);
         financeReportButton.setActionCommand("Finance Report");
 
         panel.add(goodsPageButton);
