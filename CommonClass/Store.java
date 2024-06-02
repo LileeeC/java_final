ba9e65db70a3af1dea8a012a429938b57ed7eba4
Aka2210 <qoo0973215128@gmail.com>
2024-06-03 04:42:06 +0800

    fix: Fix the bug that can't modify goods name

--- CommonClass/Store.java
+++ CommonClass/Store.java
@@ -1,6 +1,8 @@
 //attributes of store
 package CommonClass;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import javax.swing.JButton;
@@ -10,7 +12,7 @@ import Pages.GoodsPage;
 public class Store {
     public JPanel StorePanel = new JPanel();
     public Set<String> AllGoodsName = new HashSet<>();
-    public Goods[] GoodsList;
+    public Map<String, Goods> GoodsList = new HashMap<>();
     public String name;
     public JButton ButtonTrigger;
 
